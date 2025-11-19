801d2562acf16396d58e1bf1906215513680393a
Tsila3384 <robeltsilavina@gmail.com>
2025-11-19 15:09:01 +0300

    sprint3d_done

--- src/main/java/com/tsila/servlet/FrontServlet.java
+++ src/main/java/com/tsila/servlet/FrontServlet.java
@@ -10,129 +10,148 @@ import java.io.PrintWriter;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLDecoder;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 public class FrontServlet extends HttpServlet {
-    // Map qui lie les URL aux méthodes correspondantes
     private Map<String, Method> urlMappings = new HashMap<>();
     private Map<String, Class<?>> controllerMappings = new HashMap<>();
 
     @Override
     public void init() throws ServletException {
-        System.out.println(" Initialisation du framework...");
+        System.out.println("Initialisation du framework...");
 
         try {
-            // Le package à scanner peut être lu depuis web.xml
             String basePackage = getServletContext().getInitParameter("basePackage");
-            if (basePackage == null)
+            if (basePackage == null) {
                 basePackage = "com.tsila.controllers";
+            }
 
             scanControllers(basePackage);
+            exposeUrlMappings();
 
             System.out.println("Scan terminé !");
-
-            // Construire une map simple URL -> "ClassName#methodName" et la stocker dans le
-            // ServletContext
-            HashMap<String, String> urlInfo = new HashMap<>();
-            urlMappings.forEach((url, method) -> {
-                Class<?> controllerClass = controllerMappings.get(url);
-                String handler = controllerClass.getName() + "#" + method.getName();
-                urlInfo.put(url, handler);
-                System.out.println(url + " => " + handler);
-            });
-
-            // Exposer la map au contexte pour que d'autres composants puissent y accéder
-            getServletContext().setAttribute("urlMappings", urlInfo);
-
         } catch (Exception e) {
             throw new ServletException("Erreur d'initialisation du FrontServlet", e);
         }
     }
 
     private void scanControllers(String packageName) throws Exception {
+        Set<String> classNames = findClassNames(packageName);
+        registerControllers(classNames);
+    }
+
+    private Set<String> findClassNames(String packageName) throws Exception {
         String path = packageName.replace('.', '/');
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-
         Enumeration<URL> resources = classLoader.getResources(path);
         Set<String> classNames = new HashSet<>();
 
         while (resources.hasMoreElements()) {
             URL resource = resources.nextElement();
-            String protocol = resource.getProtocol();
+            classNames.addAll(scanResource(resource, path, packageName));
+        }
 
-            if ("file".equals(protocol)) {
-                // exploded classes in filesystem
-                File directory = new File(resource.toURI());
-                if (directory.exists()) {
-                    File[] files = directory.listFiles();
-                    if (files != null) {
-                        for (File file : files) {
-                            if (file.getName().endsWith(".class")) {
-                                classNames.add(packageName + "." + file.getName().replace(".class", ""));
-                            }
-                        }
-                    }
-                }
+        return classNames;
+    }
 
-            } else if ("jar".equals(protocol)) {
-                // resource like jar:file:/path/to/jar.jar!/com/tsila/controllers
-                String full = resource.getPath();
-                int excl = full.indexOf('!');
-                String jarPath = full.substring(0, excl);
-                if (jarPath.startsWith("file:"))
-                    jarPath = jarPath.substring(5);
-                jarPath = URLDecoder.decode(jarPath, "UTF-8");
-
-                try (JarFile jar = new JarFile(jarPath)) {
-                    Enumeration<JarEntry> entries = jar.entries();
-                    while (entries.hasMoreElements()) {
-                        JarEntry entry = entries.nextElement();
-                        String name = entry.getName();
-                        if (name.startsWith(path) && name.endsWith(".class") && !entry.isDirectory()) {
-                            String className = name.replace('/', '.').replace(".class", "");
-                            classNames.add(className);
-                        }
-                    }
-                }
-
+    private Set<String> scanResource(URL resource, String path, String packageName) {
+        try {
+            if ("jar".equals(resource.getProtocol())) {
+                return scanJar(resource, path);
             } else {
-                // fallback: try to treat as file
-                try {
-                    File directory = new File(resource.toURI());
-                    if (directory.exists()) {
-                        File[] files = directory.listFiles();
-                        if (files != null) {
-                            for (File file : files) {
-                                if (file.getName().endsWith(".class")) {
-                                    classNames.add(packageName + "." + file.getName().replace(".class", ""));
-                                }
-                            }
-                        }
-                    }
-                } catch (Exception ex) {
-                    System.out.println("Impossible de scanner la ressource: " + resource + " -> " + ex.getMessage());
+                return scanDirectory(resource, packageName);
+            }
+        } catch (Exception e) {
+            System.out.println("Erreur lors du scan de: " + resource + " -> " + e.getMessage());
+            return new HashSet<>();
+        }
+    }
+
+    private Set<String> scanJar(URL resource, String path) throws Exception {
+        Set<String> classNames = new HashSet<>();
+        String jarPath = extractJarPath(resource);
+
+        try (JarFile jar = new JarFile(jarPath)) {
+            Enumeration<JarEntry> entries = jar.entries();
+            while (entries.hasMoreElements()) {
+                JarEntry entry = entries.nextElement();
+                String name = entry.getName();
+
+                if (name.startsWith(path) && name.endsWith(".class") && !entry.isDirectory()) {
+                    classNames.add(convertPathToClassName(name));
                 }
             }
         }
 
+        return classNames;
+    }
+
+    private String extractJarPath(URL resource) throws Exception {
+        String fullPath = resource.getPath();
+        String jarPath = fullPath.substring(0, fullPath.indexOf('!'));
+
+        if (jarPath.startsWith("file:")) {
+            jarPath = jarPath.substring(5);
+        }
+
+        return URLDecoder.decode(jarPath, "UTF-8");
+    }
+
+    private Set<String> scanDirectory(URL resource, String packageName) throws Exception {
+        Set<String> classNames = new HashSet<>();
+        File directory = new File(resource.toURI());
+
+        if (directory.exists() && directory.isDirectory()) {
+            File[] files = directory.listFiles((dir, name) -> name.endsWith(".class"));
+
+            if (files != null) {
+                for (File file : files) {
+                    String className = packageName + "." + file.getName().replace(".class", "");
+                    classNames.add(className);
+                }
+            }
+        }
+
+        return classNames;
+    }
+
+    private String convertPathToClassName(String path) {
+        return path.replace('/', '.').replace(".class", "");
+    }
+
+    private void registerControllers(Set<String> classNames) throws Exception {
         for (String className : classNames) {
             Class<?> clazz = Class.forName(className);
+
             if (clazz.isAnnotationPresent(Controller.class)) {
-                for (Method m : clazz.getDeclaredMethods()) {
-                    if (m.isAnnotationPresent(Url.class)) {
-                        String url = m.getAnnotation(Url.class).Value();
-                        urlMappings.put(url, m);
-                        controllerMappings.put(url, clazz);
-                    }
-                }
+                registerControllerMethods(clazz);
             }
         }
+    }
+
+    private void registerControllerMethods(Class<?> clazz) {
+        for (Method method : clazz.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(Url.class)) {
+                String url = method.getAnnotation(Url.class).Value();
+                urlMappings.put(url, method);
+                controllerMappings.put(url, clazz);
+            }
+        }
+    }
+
+    private void exposeUrlMappings() {
+        Map<String, String> urlInfo = new HashMap<>();
+
+        urlMappings.forEach((url, method) -> {
+            Class<?> controllerClass = controllerMappings.get(url);
+            String handler = controllerClass.getName() + "#" + method.getName();
+            urlInfo.put(url, handler);
+            System.out.println(url + " => " + handler);
+        });
+
+        getServletContext().setAttribute("urlMappings", urlInfo);
     }
 
     @Override
@@ -140,11 +159,11 @@ public class FrontServlet extends HttpServlet {
             throws ServletException, IOException {
 
         String path = req.getPathInfo();
-        PrintWriter out = resp.getWriter();
         resp.setContentType("text/plain");
+        PrintWriter out = resp.getWriter();
 
         if (path == null) {
-            out.println(" Aucune URL fournie");
+            out.println("Aucune URL fournie");
             return;
         }
 
@@ -153,8 +172,8 @@ public class FrontServlet extends HttpServlet {
             out.println("Aucune méthode correspondante pour " + path);
         } else {
             Class<?> controller = controllerMappings.get(path);
-            out.println(" Classe: " + controller.getName());
-            out.println(" Méthode: " + method.getName());
+            out.println("Classe: " + controller.getName());
+            out.println("Méthode: " + method.getName());
         }
     }
 }
